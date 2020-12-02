62a5e8a4f3443b43a833d44d9374151c8c6796f4
Jacob Bathan <jacobbathan@gmail.com>
2020-12-01 20:49:31 -0800

    part 2 added

--- 1/1.py
+++ 1/1.py
@@ -7,14 +7,16 @@ def expenseReport():
     report.close()
     for item in report_list:
         cast_report_list.append(int(item))
-    
+
     values = []
 
     values = twoSumExpense(cast_report_list, 2020)
 
     print(values[0] * values[1])
 
+    three_values = threeSumExpense(cast_report_list, 2020)
 
+    print(three_values[0][0] * three_values[0][1] * three_values[0][2])
 
 
 def twoSumExpense(array, target):
@@ -22,6 +24,32 @@ def twoSumExpense(array, target):
         for j in range(len(array)):
             if array[i] + array[j] == target:
                 return [array[i], array[j]]
+
+
+def threeSumExpense(array, target):
+    output = []
+    array.sort()
+    length = len(array)
+    for i in range(length):
+        left = i + 1
+        right = length - 1
+        if array[i - 1] == array[i]: continue
+        while left < right:
+            total = array[i] + array[left] + array[right]
+            if total == target:
+                output.append([array[i], array[left], array[right]])
+                while left < right and array[left] == array[left + 1]:
+                    left += 1
+                while left < right and array[right] == array[right - 1]:
+                    right -= 1
+                left, right = left + 1, right - 1
+            elif total < target:
+                left += 1
+            else:
+                right -= 1
+    return output
+
+
         
 
 expenseReport()
